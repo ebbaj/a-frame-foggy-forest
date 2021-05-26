aa7cbcda82cb64bfd4b267440b5ca1939f1b354a
ebbaj <ebba-jonsson@hotmail.com>
2021-05-26 15:36:23 +0200

    Removed commented code

--- src/js/box-game.js
+++ src/js/box-game.js
@@ -1,11 +1,3 @@
-/*
-let boxCount = document.createElement("div");
-let boxCountContainer = document.getElementById("box-count-container");
-
-boxCount.appendChild(countParagraph);
-boxCountContainer.appendChild(boxCount);
-*/
-
 //Test click event on all of the boxes
 AFRAME.registerComponent("open-box", {
   init: function () {
@@ -26,24 +18,6 @@ AFRAME.registerComponent("open-box", {
     this.el.removeEventListener("click", this.openBox);
   },
 });
-/*
-AFRAME.registerComponent("opened-boxes-popup", {
-  init: function () {
-    let el = this.el;
-    let boxesPopup = document.querySelector(".boxes-popup");
-    let boxesScene = document.querySelector(".boxes-scene");
-    this.openedBoxesPopup = function () {
-      boxesPopup.classList.remove("hide");
-      boxesScene.classList.add("layer");
-    };
-    this.el.addEventListener("click", this.openedBoxesPopup);
-    game();
-  },
-  remove: function () {
-    this.el.removeEventListener("click", this.openedBoxesPopup);
-  },
-});
-*/
 
 function boxGame() {
   const box1 = document.getElementById("box-1");
